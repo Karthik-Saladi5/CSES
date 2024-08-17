4edc59f6f23e3220179683f1993bceccff7982f0
SK's Laptop <karthiksaladi6@gmail.com>
2024-08-17 15:35:14 +0530

    solved

---
+++ weirdAlgorithm.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+#define pb push_back
+#define F first
+#define S second
+#define SQ(a) (a)*(a)
+#define REP(i,a,b) for(int i=a;i<=b;i++)
+#define ALL(a) (a).begin(), (a).end()
+const int mod = 1000000007;
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define tc\
+    int t;\
+    cin>>t;\
+    while(t--)
+void takeinput(){
+    #ifndef ONLINE_JUDGE
+    freopen("D:\\coding\\input.txt","r",stdin);
+    freopen("D:\\coding\\output.txt","w",stdout);
+    #endif
+}
+void sk(){
+    // Your code goes here
+    ll n; cin >> n;
+    while (n != 1) {
+        cout << n << " ";
+        if (n & 1) n = n * 3 + 1;
+        else n /= 2;
+    }
+    cout << n;
+}
+
+int main()
+{
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+   takeinput();
+    sk();
+    return 0;
+}
